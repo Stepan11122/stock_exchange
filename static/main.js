c4b613bd317064bad02738a37c96d9d93ecbd477
GreenCherry1 <137512652+GreenCherry1@users.noreply.github.com>
2025-05-13 19:49:25 +0300

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,12 +1,11 @@
 import { renderGame } from './ui.js';
-
+import { initCards } from './canvas.js';
 console.log("main.js loaded");
-
 export function initGame(player) {
     const socket = io();
     socket.emit('join', { player });
-
     socket.on('update_state', data => {
+        initCards(['5+', 'Q-', 'J+', '10-']);
         renderGame(data, player, socket);
     });
 }
